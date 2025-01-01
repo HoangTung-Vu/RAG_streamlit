9f58a4a1b547225539d0fc02db01ffcf979f46d0
HoangTung-Vu <minhhoangtungvu04@gmail.com>
2025-01-01 16:15:10 +0700

    compatible yet ?

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+__import__('pysqlite3') 
+import sys 
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import os
 import tempfile
 from dotenv import load_dotenv
@@ -9,6 +12,7 @@ from rag_utils import (
     initialize_llm,
     create_rag_chain
 )
+
 
 # Load environment variables
 load_dotenv()
